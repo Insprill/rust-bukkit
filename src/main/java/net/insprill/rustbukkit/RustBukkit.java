ecb69182f4223af720332e27f43636e5ca6615c5
Pierce Thompson <insprill@gmail.com>
2022-10-13 18:07:46 -0400

    Get basic hello native call working

--- src/main/java/net/insprill/rustbukkit/RustBukkit.java
+++ src/main/java/net/insprill/rustbukkit/RustBukkit.java
@@ -2,11 +2,25 @@ package net.insprill.rustbukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class RustBukkit extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        File lib = new File(getDataFolder(), "rust_bukkit.dll");
+        try {
+            System.load(lib.getAbsolutePath());
+        } catch (UnsatisfiedLinkError e) {
+            if (!e.getMessage().contains("already loaded")) {
+                getLogger().severe("Failed to load native library.");
+                getServer().getPluginManager().disablePlugin(this);
+                return;
+            }
+        }
+        getLogger().info(hello("Insprill"));
     }
+
+    private static native String hello(String str);
 
 }
