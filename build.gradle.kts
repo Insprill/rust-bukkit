65b8307c8030f225f76e11db232dbae518149014
Pierce Thompson <insprill@gmail.com>
2022-10-13 17:08:07 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import java.io.ByteArrayOutputStream
+
+plugins {
+    id("java")
+}
+
+group = "net.insprill"
+version = getFullVersion()
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // Spigot-API
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        filesMatching("plugin.yml") {
+            expand("version" to version)
+        }
+    }
+}
+
+fun getFullVersion(): String {
+    val version = project.property("version")!! as String
+    return if (version.contains("-SNAPSHOT")) {
+        "$version+rev.${getGitHash()}"
+    } else {
+        version
+    }
+}
+
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "--verify", "--short", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
+}
---
+++ src/main/java/net/insprill/rustbukkit/RustBukkit.java
@@ -0,0 +1,12 @@
+package net.insprill.rustbukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RustBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
