c44156c84beb16433d097948eed5503c95a5fea6
Pierce Thompson <insprill@gmail.com>
2022-10-13 22:03:02 -0400

    Setup test plugin with cargo project

--- test-plugin/build.gradle.kts
+++ test-plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.rikonardo.papermake") version "1.0.4"
 }
 
@@ -23,10 +24,36 @@ tasks {
             expand("version" to version)
         }
     }
+    compileJava {
+        options.compilerArgs.addAll(listOf("-h", "${file("${buildDir}/native")}"))
+    }
+    shadowJar {
+        archiveClassifier.set("")
+        from(buildRustLib())
+    }
+    build {
+        dependsOn(shadowJar)
+    }
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
+}
+
+fun buildRustLib(): File? {
+    val rustDir = file("rust")
+    exec {
+        commandLine("cargo", "build", "--release", "--manifest-path=$rustDir/Cargo.toml")
+    }
+    val releaseDir = File(rustDir.absolutePath, "target/release")
+    for (file in releaseDir.listFiles()!!) {
+        when (file.name) {
+            "test_plugin.dll" -> return file
+            "test_plugin.so" -> return file
+            "test_plugin.dylib" -> return file
+        }
+    }
+    return null
 }
