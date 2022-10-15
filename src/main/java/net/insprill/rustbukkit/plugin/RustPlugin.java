cace009d632762b7e7d854d443f676db14d7e27b
Pierce Thompson <insprill@gmail.com>
2022-10-15 11:33:27 -0400

    Register plugin commands on enable

--- src/main/java/net/insprill/rustbukkit/plugin/RustPlugin.java
+++ src/main/java/net/insprill/rustbukkit/plugin/RustPlugin.java
@@ -1,13 +1,31 @@
 package net.insprill.rustbukkit.plugin;
 
 import net.insprill.rustbukkit.RustBukkit;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.logging.Level;
 
 public abstract class RustPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        RustBukkit.getPluginManager().registerPlugin(this);
+        RustBukkit rustBukkit = RustBukkit.getInstance();
+        rustBukkit.getPluginManager().registerPlugin(this);
+
+        try {
+            getCommands().parallelStream()
+                    .map(cmd -> getCommand(cmd.getName()))
+                    .filter(Objects::nonNull)
+                    .forEach(cmd -> cmd.setExecutor(rustBukkit.getCommandHandler()));
+        } catch (ReflectiveOperationException e) {
+            getLogger().log(Level.SEVERE, "Failed to register commands", e);
+        }
     }
 
     @Override
@@ -21,6 +39,15 @@ public abstract class RustPlugin extends JavaPlugin {
     }
 
     public void disable() {
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<Command> getCommands() throws ReflectiveOperationException {
+        Class<? extends Server> serverClass = getServer().getClass();
+        Field commandMap = serverClass.getDeclaredField("commandMap");
+        commandMap.setAccessible(true);
+        SimpleCommandMap simpleCommandMap = (SimpleCommandMap) commandMap.get(getServer());
+        return simpleCommandMap.getCommands();
     }
 
 }
