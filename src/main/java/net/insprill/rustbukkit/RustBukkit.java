f4a767236ce7db444d7d0138ab86b93c849e64f0
Pierce Thompson <insprill@gmail.com>
2022-10-15 11:33:07 -0400

    Refactor RustBukkit - Make plugin manager not static - Added singleton - Added getter for the command handler

--- src/main/java/net/insprill/rustbukkit/RustBukkit.java
+++ src/main/java/net/insprill/rustbukkit/RustBukkit.java
@@ -1,11 +1,25 @@
 package net.insprill.rustbukkit;
 
+import net.insprill.rustbukkit.command.RustCommandHandler;
 import net.insprill.rustbukkit.plugin.RustPlugin;
 import net.insprill.rustbukkit.plugin.RustPluginManager;
 
 public final class RustBukkit extends RustPlugin {
 
-    private static RustPluginManager pluginManager;
+    // region Singleton
+    private static RustBukkit instance;
+
+    private static void setInstance(RustBukkit instance) {
+        RustBukkit.instance = instance;
+    }
+
+    public static RustBukkit getInstance() {
+        return RustBukkit.instance;
+    }
+    // endregion
+
+    private RustPluginManager pluginManager;
+    private RustCommandHandler commandHandler;
 
     @Override
     public String libraryName() {
@@ -14,16 +28,18 @@ public final class RustBukkit extends RustPlugin {
 
     @Override
     public void onEnable() {
-        setPluginManager(new RustPluginManager(this));
+        setInstance(this);
+        this.pluginManager = new RustPluginManager(this);
+        this.commandHandler = new RustCommandHandler();
         super.onEnable();
     }
 
-    public static RustPluginManager getPluginManager() {
-        return RustBukkit.pluginManager;
+    public RustPluginManager getPluginManager() {
+        return this.pluginManager;
     }
 
-    private static void setPluginManager(RustPluginManager pluginManager) {
-        RustBukkit.pluginManager = pluginManager;
+    public RustCommandHandler getCommandHandler() {
+        return this.commandHandler;
     }
 
     @Override
