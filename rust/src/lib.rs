d8cc829d09247c413d3ddf296379a34fa7c9c2bd
Pierce Thompson <insprill@gmail.com>
2022-10-15 11:34:25 -0400

    Add static RustBukkit instance to rust lib

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,10 +1,34 @@
 use jni::JNIEnv;
 use jni::objects::{JObject};
+use crate::bukkit::command::Command;
 
 pub mod bukkit;
+
+struct RustBukkit {
+    commands: Vec<Command>,
+}
+
+impl RustBukkit {
+    fn new() -> Self {
+        Self {
+            commands: Vec::new()
+        }
+    }
+}
+
+static mut RUST_BUKKIT: Option<RustBukkit> = None;
 
 #[no_mangle]
 pub extern "system" fn Java_net_insprill_rustbukkit_RustBukkit_enable(env: JNIEnv, _obj: JObject) {
     let jvm = env.get_java_vm().expect("Failed to get JVM");
     jvm.attach_current_thread_permanently().expect("Failed to attach to JVM thread");
+    unsafe {
+        RUST_BUKKIT = Some(RustBukkit::new())
+    }
+}
+
+pub(crate) fn get_bukkit() -> &'static mut RustBukkit {
+    unsafe {
+        RUST_BUKKIT.as_mut().expect("get_instance called before enable!")
+    }
 }
