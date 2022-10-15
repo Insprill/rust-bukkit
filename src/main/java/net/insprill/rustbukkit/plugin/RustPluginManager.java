24361fa9fa6b777f04b3ce5d35975930846c996a
Pierce Thompson <insprill@gmail.com>
2022-10-15 16:29:55 -0400

    Use System#mapLibraryName for finding the proper library extension

--- src/main/java/net/insprill/rustbukkit/plugin/RustPluginManager.java
+++ src/main/java/net/insprill/rustbukkit/plugin/RustPluginManager.java
@@ -13,26 +13,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Set;
 import java.util.logging.Level;
 
 public class RustPluginManager {
-
-    private static final String PLATFORM_LIB_EXTENSION;
-
-    static {
-        String os = System.getProperty("os.name").toLowerCase(Locale.ROOT);
-        if (os.contains("windows")) {
-            PLATFORM_LIB_EXTENSION = "dll";
-        } else if (os.contains("mac")) {
-            PLATFORM_LIB_EXTENSION = "dylib";
-        } else if (os.contains("linux")) {
-            PLATFORM_LIB_EXTENSION = "so";
-        } else {
-            throw new UnsupportedOperationException("Unknown platform");
-        }
-    }
 
     private final File cacheDir;
     private final JavaPlugin rootPlugin;
@@ -71,7 +55,7 @@ public class RustPluginManager {
     }
 
     private void loadPluginLib(RustPlugin plugin) {
-        try (InputStream resource = plugin.getResource(plugin.libraryName() + "." + PLATFORM_LIB_EXTENSION)) {
+        try (InputStream resource = plugin.getResource(System.mapLibraryName(plugin.libraryName()))) {
             if (resource == null) {
                 throw new NativeLibraryException("Failed to find native library for " + plugin.getName() + ". Does it support this platform?");
             }
@@ -85,7 +69,7 @@ public class RustPluginManager {
     @SuppressWarnings("UnstableApiUsage")
     private File cacheNativeLibrary(InputStream library, RustPlugin plugin) {
         try {
-            File dest = new File(this.cacheDir, plugin.getName() + "." + PLATFORM_LIB_EXTENSION);
+            File dest = new File(this.cacheDir, System.mapLibraryName(plugin.getName()));
             if (!dest.exists() && !dest.createNewFile()) {
                 throw new FileException("Failed to create cache file");
             }
